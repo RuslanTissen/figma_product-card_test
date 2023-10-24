cb7b4068e46ebc547d705625ffb1327b51e849ae
SFERAS <rthyssen73@gmail.com>
2023-10-24 09:41:30 -0300

    24 okt. first one

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import "./Card.scss"
-import image from "../images/figma_image.png"
+// import image from "../images/figma_image.png"
 import image2 from "../images/figma_image-2.png"
+import { MdOutlineLocationOn } from "react-icons/md"
+import { GoDotFill } from "react-icons/go"
+
 
 function Card() {
 	return (
@@ -12,7 +15,25 @@ function Card() {
 				</div>
 
 				<div className="card__container--content">
-					zmdfnvkjsadbfvkññfkghlrkjhlrtkhjrlthkj
+					<div className="card__container--content-box1">
+						<p className='card__content-box1--title'>Fruits and vegetables are packed with essential vitamins, minerals, and nutrients that are essential for maintaining a healthy body and mind</p>
+						<div className="card__content-box1--lable-box">
+							<div className="lable-container">
+								<p className='lable__text'> Fruits and vegetables</p>
+								<p className='lable__tag'>Fresh <GoDotFill /> Healthy food</p>
+							</div>
+							<div className="card__content-box1--lable__price-box">
+								<p className="lable__price">$ 3.90</p>
+							</div>
+						</div>
+
+
+					</div>
+
+					<div className="card__container--content-box2">
+						<MdOutlineLocationOn />
+						<p>Newport Beach, CA, USA</p>
+					</div>
 				</div>
 			</div>
 
