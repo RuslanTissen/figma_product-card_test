7b51dcf9198a9560ac40113c1604fcd4bf25c512
SFERAS <rthyssen73@gmail.com>
2023-10-24 10:01:12 -0300

    data nearly done

---
+++ src/components/Data.js
@@ -0,0 +1,39 @@
+const data = {
+	card_0: {
+
+	},
+	card_1: {
+		image: "Services",
+		title: "How we",
+		subtitle_bold: "carreers",
+		tages: "about us",
+		price: "contact us",
+		location: "estimate your project",
+	},
+	card_2: {
+		image: "Services",
+		title: "How we",
+		subtitle_bold: "carreers",
+		tages: "about us",
+		price: "contact us",
+		location: "estimate your project",
+	},
+	card_3: {
+		image: "Services",
+		title: "How we",
+		subtitle_bold: "carreers",
+		tages: "about us",
+		price: "contact us",
+		location: "estimate your project",
+	},
+	card_4: {
+		image: "Services",
+		title: "How we",
+		subtitle_bold: "carreers",
+		tages: "about us",
+		price: "contact us",
+		location: "estimate your project",
+	}
+}
+
+export default data
